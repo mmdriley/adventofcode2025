b8b3636c0b9b65d19434080ad19035a0bce36d93
Matthew Riley <mdriley@gmail.com>
2025-12-02 19:17:41 -0800

    Day 2, faster

--- 02/02.py
+++ 02/02.py
@@ -1,5 +1,27 @@
+import math
+
+
+def repeated_numbers(limit):
+    r = set()
+    max_len = math.floor(math.log10(limit) + 1)
+
+    for i in range(1, 10 ** (max_len // 2)):
+        width = math.floor(math.log10(i) + 1)
+        stride = 10 ** width
+
+        n = stride * i + i
+        while n <= limit:
+            r.add(n)
+            n = stride * n + i
+
+    return r
+
+
 # N is <s><s>
 def is_interesting1(n):
+    if n not in repeated:
+        return False
+
     s = str(n)
     l = len(s)
 
@@ -9,24 +31,15 @@ def is_interesting1(n):
     return s[:l//2] == s[l//2:]
 
 
-# N is <s>{2,}
-def is_interesting2(n):
-    s = str(n)
-    l = len(s)
-
-    for k in range(1, l//2 + 1):
-        if l % k != 0:
-            continue
-        d = l // k
-
-        if s == s[:k] * d:
-            return True
-
-    return False
-
-
 with open('02.txt') as f:
     contents = f.read().strip()
+
+max_n = 0
+for r in contents.split(','):
+    start, end = [int(x) for x in r.split('-')]
+    max_n = max(max_n, start, end)
+
+repeated = repeated_numbers(max_n)
 
 total1 = 0
 total2 = 0
@@ -34,11 +47,11 @@ total2 = 0
 for r in contents.split(','):
     start, end = [int(x) for x in r.split('-')]
     for i in range(start, end + 1):
-        if is_interesting1(i):
-            total1 += i
+        if i in repeated:
             total2 += i
-        elif is_interesting2(i):
-            total2 += i
+
+            if is_interesting1(i):
+                total1 += i
 
 print(total1)
 print(total2)
