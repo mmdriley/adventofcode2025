21c7565b7628d9742b84e9eb580701aaaea5f613
Matthew Riley <mdriley@gmail.com>
2025-12-03 21:47:31 -0800

    Successful day 4

---
+++ 04/04.py
@@ -0,0 +1,58 @@
+grid = []
+
+with open('04.txt') as f:
+    for l in f:
+        l = l.strip()
+        if not l:
+            continue
+        grid = grid + [[' '] + list(l) + [' ']]
+
+pad = [' '] * len(grid[0])
+grid = [pad] + grid + [pad]
+
+
+adjacencies = [
+    (-1, -1),
+    (-1,  0),
+    (-1,  1),
+    ( 0, -1),
+    ( 0,  1),
+    ( 1, -1),
+    ( 1,  0),
+    ( 1,  1),
+]
+
+
+def remove_round(grid: list[list[str]]) -> int:
+    removed = []
+
+    for i in range(1, len(grid) - 1):
+        for j in range(1, len(grid[0]) - 1):
+            if grid[i][j] != '@':
+                continue
+
+            present = 0
+            for dx, dy in adjacencies:
+                if grid[i + dx][j + dy] == '@':
+                    present += 1
+            if present < 4:
+                removed.append((i, j))
+    
+    for i, j in removed:
+        grid[i][j] = '.'
+
+    return len(removed)
+
+
+# part 1
+n = remove_round(grid)
+print(n)
+
+# part 2
+while True:
+    r = remove_round(grid)
+    n += r
+    if r == 0:
+        break
+
+print(n)
