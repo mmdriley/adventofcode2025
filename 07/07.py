b596e45c9f49697d9eb42caa4e6ab0565a48a1d8
Matthew Riley <mdriley@gmail.com>
2025-12-07 00:31:50 -0800

    Slightly shorter day 7 solution

--- 07/07.py
+++ 07/07.py
@@ -1,48 +1,40 @@
+from collections import defaultdict
+
 with open('07.txt') as f:
     grid = [l.strip('\n') for l in f.readlines()]
 
 grid = [list(l) for l in grid]
 
-paths: dict[tuple[int, int], int] = dict()
-for i in range(len(grid)):
-    for j in range(len(grid[0])):
-        paths[(i,j)] = 0
 
+splitters_hit: set[tuple[int, int]] = set()
+paths: dict[tuple[int, int], int] = defaultdict(int)
 for j, c in enumerate(grid[0]):
-    paths[(0, j)] = 1 if grid[0][j] == 'S' else 0
+    if c == 'S':
+        paths[(0,j)] = 1
+
 
 # propagate beams
 for i, l in enumerate(grid[1:], start=1):
     for j, c in enumerate(l):
-        if grid[i-1][j] not in ('S', '|'):
-            assert paths[(i-1,j)] == 0
-            # no beam to propagate
+        if paths[(i-1,j)] == 0:
+            # no incoming beam, nothing to do
             continue
 
-        assert paths[(i-1,j)] > 0
-
-        if c in ('.', '|'):
-            grid[i][j] = '|'
+        if c == '.':
             paths[(i,j)] += paths[(i-1,j)]
         elif c == '^':
+            splitters_hit.add((i, j))
             if j - 1 >= 0:
-                assert grid[i][j-1] in ('.', '|')
-                grid[i][j-1] = '|'
+                assert grid[i][j-1] == '.'
                 paths[(i, j-1)] += paths[(i-1,j)]
             if j + 1 < len(grid[i]):
-                assert grid[i][j+1] in ('.', '|')
-                grid[i][j+1] = '|'
+                assert grid[i][j+1] == '.'
                 paths[(i, j+1)] += paths[(i-1,j)]
 
-# count splits
-splits = 0
-for i in range(1, len(grid)):
-    for j in range(len(grid[0])):
-        if grid[i][j] == '^' and grid[i-1][j] in ('S', '|'):
-            splits += 1
+# part 1
+print(len(splitters_hit))
 
-print(splits)
-
+# part 2
 total_paths = 0
 for j in range(len(grid[0])):
     total_paths += paths[(len(grid)-1, j)]
