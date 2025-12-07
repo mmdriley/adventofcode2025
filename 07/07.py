f052a6d955418ac4a99b722da0d292940ee3f345
Matthew Riley <mdriley@gmail.com>
2025-12-07 00:16:20 -0800

    Successful day 7

--- 07/07.py
+++ 07/07.py
@@ -3,25 +3,36 @@ with open('07.txt') as f:
 
 grid = [list(l) for l in grid]
 
-# propagate beams
-for i, l in enumerate(grid):
-    if i == 0:
-        continue
+paths: dict[tuple[int, int], int] = dict()
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        paths[(i,j)] = 0
 
+for j, c in enumerate(grid[0]):
+    paths[(0, j)] = 1 if grid[0][j] == 'S' else 0
+
+# propagate beams
+for i, l in enumerate(grid[1:], start=1):
     for j, c in enumerate(l):
         if grid[i-1][j] not in ('S', '|'):
+            assert paths[(i-1,j)] == 0
             # no beam to propagate
             continue
-        
-        if c == '.':
+
+        assert paths[(i-1,j)] > 0
+
+        if c in ('.', '|'):
             grid[i][j] = '|'
+            paths[(i,j)] += paths[(i-1,j)]
         elif c == '^':
             if j - 1 >= 0:
                 assert grid[i][j-1] in ('.', '|')
                 grid[i][j-1] = '|'
+                paths[(i, j-1)] += paths[(i-1,j)]
             if j + 1 < len(grid[i]):
                 assert grid[i][j+1] in ('.', '|')
                 grid[i][j+1] = '|'
+                paths[(i, j+1)] += paths[(i-1,j)]
 
 # count splits
 splits = 0
@@ -31,3 +42,9 @@ for i in range(1, len(grid)):
             splits += 1
 
 print(splits)
+
+total_paths = 0
+for j in range(len(grid[0])):
+    total_paths += paths[(len(grid)-1, j)]
+
+print(total_paths)
